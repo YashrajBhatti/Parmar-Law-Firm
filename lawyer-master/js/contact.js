b8325e78326e3d8054d811bd7c8db15a1259fe22
YashrajBhatti <w0832320@myscc.ca>
2024-03-07 20:23:57 -0500

    1st commit

---
+++ lawyer-master/js/contact.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                subject: {
+                    required: true,
+                    minlength: 4
+                },
+                number: {
+                    required: true,
+                    minlength: 5
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please Enter a name.",
+                    minlength: "Name must be minimum 3 characters"
+                },
+                subject: {
+                    required: "Please purpose of you visit.",
+                    minlength: "Subject must be  at least 4 characters long."
+                },
+                number: {
+                    required: "Please enter a number.",
+                    minlength: "your Number must consist of at least 10 characters."
+                },
+                email: {
+                    required: "Please provide an Email address.",
+                },
+                message: {
+                    required: "Please tell us in detail for your visit.",
+                    minlength: "Minimum 20  Characters are needed."
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
